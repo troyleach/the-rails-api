96c4ef497edbb9431ac6a26153a7fe992ca55739
Troy Leach <troyleach29@gmail.com>
2020-11-01 22:56:04 -0600

    fixed type in the todo_param method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,6 +29,6 @@ class TodosController < ApplicationController
   private
 
   def todo_param
-    params.require(:todo).permit(:title, :done)
+    params.require(:todo).permit(:title, :completed)
   end
 end
