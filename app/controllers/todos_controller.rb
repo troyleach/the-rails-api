83540ab360f580f61c10a1eefa46918f3684684e
Troy Leach <troyleach29@gmail.com>
2020-11-08 10:53:56 -0600

    WIP

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,16 @@
 class TodosController < ApplicationController
   def index
     todos = Todo.order("created_at DESC")
+    # FIXME: right now this returns all todos, most likely only want yesterdays and todays
+    # FIXME: also it takes the time into account so there is a key for every todo created in a 24 hour span
+
+    # list.group_by(&:group_by_assign_date).map {|k,v| [k, v.length]}.sort
+    # list.group_by(&:group_by_assign_date).map {|k,v| [k, v.length]}.sort
+    if params[:type]
+      # I don't like that I am overwriting the variable
+      fetch_type = params[:type].to_sym
+      todos = todos.group_by(&fetch_type)
+    end
     render json: todos
   end
 
@@ -29,6 +39,6 @@ class TodosController < ApplicationController
   private
 
   def todo_param
-    params.require(:todo).permit(:title, :completed)
+    params.require(:todo).permit(:title, :completed, :assign_date)
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,15 @@
 class Todo < ApplicationRecord
+  # validate :validate_assign_date
+
+  def group_by_assign_date
+    # FIXME: this does not take into account timezone
+    # Time.zone.at
+    assign_date.localtime.to_date
+    # assign_date.to_date.to_s(:db)
+  end
+
+  private
+  def validate_assign_date
+    puts 'I am validing the date'
+  end
 end
