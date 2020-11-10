11f43648bca0a7c5235805884978597d22b380a2
Troy Leach <troyleach29@gmail.com>
2020-11-09 21:06:14 -0600

    WIP a bit of code clean up

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,10 +2,7 @@ class Todo < ApplicationRecord
   # validate :validate_assign_date
 
   def group_by_assign_date
-    # FIXME: this does not take into account timezone
-    # Time.zone.at
     assign_date.localtime.to_date
-    # assign_date.to_date.to_s(:db)
   end
 
   private
