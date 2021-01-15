db0be4b44819812f21b953e85e8dfca50ad1b5df
Troy Leach <troyleach29@gmail.com>
2021-01-14 21:26:39 -0600

    made a fix for the returning the date in the right timezone

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,8 +4,12 @@ class Todo < ApplicationRecord
   def group_by_assign_date
     puts '!* ' * 15
     puts "before modificaiton #{assign_date}"
-    puts assign_date.localtime.to_date
-    assign_date.localtime.to_date
+    zone = ActiveSupport::TimeZone.new("Central Time (US & Canada)")
+    # puts assign_date.localtime.to_date
+    # assign_date.localtime.to_date
+    date_in_timezone = assign_date.in_time_zone(zone).strftime('%Y-%m-%d')
+    puts "after modificaiton #{date_in_timezone}"
+    date_in_timezone
   end
 
   private
