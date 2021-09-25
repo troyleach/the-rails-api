5d0885b556246439d683cfbd411bdf800cf50e6a
Troy Leach <troyleach29@gmail.com>
2021-09-25 08:21:43 -0600

    change origin

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,12 @@ module TheRailsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    # origins 'localhost:3001', 'https://dev-dash-board.herokuapp.com'
     config.api_only = true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3001', 'https://dev-dash-board.herokuapp.com'
+        origins '*'
         resource(
           '*',
           headers: :any,
