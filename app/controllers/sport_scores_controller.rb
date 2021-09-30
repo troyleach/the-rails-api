b9f6b92fab1b35d2bc609dbf2cba9d20f6304c58
Troy Leach <troyleach29@gmail.com>
2021-09-29 21:03:53 -0600

    a few review comment changes

--- app/controllers/sport_scores_controller.rb
+++ app/controllers/sport_scores_controller.rb
@@ -9,27 +9,13 @@ class SportScoresController < Base
 
   def index
     # TODO: need to deal with errors or the wrong key
+    # fixme: api should just return all the leauges
+    # make route for each league
     league = request.headers['x-league']
     data = GetSportScores.get_scores(league)
     formated_data = build_payload(data)
-    date = formated_data[0][:date]
-
-    # Time.parse(formated_data.last[:date]).to_date
-    
-    # Time.zone.parse("2010-01-01 12:30:00").utc
-
-    # schedules_by_date = formated_data.group_by { |schedule| Time.zone.parse(schedule[:date]).utc }
-    # schedules_by_date = formated_data.group_by { |schedule| Time.parse(schedule[:date]).to_date }
-    # Time.parse(date).utc.in_time_zone('Mountain Time (US & Canada)')
-    # Time.parse(schedule[:date]).utc.in_time_zone('Mountain Time (US & Canada)')
-    # this works below ===== ugly shit
-    # schedules_by_date = formated_data.group_by { |schedule| Time.parse(schedule[:date]).utc.in_time_zone('Mountain Time (US & Canada)').to_date }
-    schedules_by_date = formated_data.group_by { |schedule| Time.parse(schedule[:date]).utc.in_time_zone('Mountain Time (US & Canada)') }
-      # .sort_by { |date| date.itself  }
-    
 
     render json: formated_data, status: 200
-    # render json: schedules_by_date, status: 200
   end
 
   def get_date(date_string)
@@ -54,10 +40,5 @@ class SportScoresController < Base
     # FIXME: deal with errors
     render json: schedule, status: 200
   end
-
-  def create; end
-
-
-  def destroy; end
 
 end
